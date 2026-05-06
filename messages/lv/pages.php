35239e58b18b6798c2cb3bbcc49f8c95844a8f10
Edgars Jēkabsons <edgars.jekabsons@gmail.com>
2026-05-06 14:30:05 +0300

    Translate public-facing UI files to Latvian
    Covers the strings visible to non-staff visitors:
    - base.php: chrome (nav, footer, errors, ARIA)
    - user.php: login/register/recovery/account
    - pages.php: default CMS page bodies (welcome, help, privacy, ...)
    - motion.php, amend.php: motion + amendment view/edit
    - comment.php: commenting UI
    - voting.php: voting UI
    
    Conventions:
    - Formal address: "Jūs" (capitalized)
    - motion = "priekšlikums", amendment = "grozījums"
    - consultation = "apspriešana"
    - proposed procedure = "ierosinātā procedūra" (not "ieteiktā",
      to avoid the recommendation/admin-guidance reading)
    
    Out of scope (still English): admin.php, manager.php, wizard.php,
    structure.php, consultation.php, diff.php, export.php, initiator.php,
    speech.php — to be translated separately.

--- messages/lv/pages.php
+++ messages/lv/pages.php
@@ -1,78 +1,78 @@
 <?php
 
 return [
-    'list_title'      => 'Content pages',
-    'list_add_std'    => 'Create standard page',
-    'list_add_custom' => 'Create a new page',
-    'list_edit'       => 'Edit pages',
-    'list_all_cons'   => 'All consultations',
-    'list_this_con'   => 'Only this consultation',
+    'list_title'      => 'Satura lapas',
+    'list_add_std'    => 'Izveidot standarta lapu',
+    'list_add_custom' => 'Izveidot jaunu lapu',
+    'list_edit'       => 'Rediģēt lapas',
+    'list_all_cons'   => 'Visas apspriešanas',
+    'list_this_con'   => 'Tikai šī apspriešana',
 
-    'content_privacy_title' => 'Privacy statement',
-    'content_privacy_bread' => 'Privacy',
-    'content_welcome'       => 'Welcome',
-    'content_welcome_text'  => '<p>Welcome to Antragsgrün</p>',
-    'content_imprint_title' => 'Imprint',
-    'content_imprint_bread' => 'Imprint',
-    'content_help_title'    => 'Help',
-    'content_help_bread'    => 'Help',
-    'content_help_place'    => '<p>Help...</p>',
-    'content_maint_bread'   => 'Maintenance',
-    'content_maint_title'   => 'Maintenance mode',
-    'content_maint_text'    => '<p>This consultation has not, yet, been made public.</p>',
-    'content_login_pre'     => 'Login (above)',
-    'content_login_post'    => 'Login (below)',
-    'content_feeds_title'   => 'RSS-Feeds',
-    'content_feeds_text'    => '<p>RSS-Feeds are a comfortable way to keep up to date with many different news sources.</p>
-        <p>You use a RSS-reader program to subscribe to all news sites, blogs, web-comics, or Antragsgrün-sites that are of interest to you.</p>
-        <p>From then on this program will show you an overview of all new articles on these sites.</p>
-        <p>A small selection of programs and web services to read RSS feeds:</p>
+    'content_privacy_title' => 'Privātuma paziņojums',
+    'content_privacy_bread' => 'Privātums',
+    'content_welcome'       => 'Sveicināti',
+    'content_welcome_text'  => '<p>Laipni lūgti Antragsgrün</p>',
+    'content_imprint_title' => 'Kontaktinformācija',
+    'content_imprint_bread' => 'Kontaktinformācija',
+    'content_help_title'    => 'Palīdzība',
+    'content_help_bread'    => 'Palīdzība',
+    'content_help_place'    => '<p>Palīdzība...</p>',
+    'content_maint_bread'   => 'Tehniskā apkope',
+    'content_maint_title'   => 'Tehniskās apkopes režīms',
+    'content_maint_text'    => '<p>Šī apspriešana vēl nav padarīta publiska.</p>',
+    'content_login_pre'     => 'Pieteikšanās (virs)',
+    'content_login_post'    => 'Pieteikšanās (zem)',
+    'content_feeds_title'   => 'RSS plūsmas',
+    'content_feeds_text'    => '<p>RSS plūsmas ir ērts veids, kā sekot līdzi daudziem dažādiem ziņu avotiem.</p>
+        <p>Jūs izmantojat RSS lasītāju programmu, lai abonētu visas ziņu vietnes, blogus, tīmekļa komiksus vai Antragsgrün vietnes, kas Jūs interesē.</p>
+        <p>Pēc tam šī programma rāda visu šajās vietnēs publicēto jauno rakstu pārskatu.</p>
+        <p>Neliela RSS plūsmu lasīšanas programmu un tīmekļa pakalpojumu izlase:</p>
         <ul>
             <li><a href="https://support.mozilla.org/en-US/kb/how-subscribe-news-feeds-and-blogs">Mozilla Thunderbird</a> (Linux, macOS, Windows)</li>
             <li><a href="https://netnewswire.com/">NetNewsWire</a> (macOS, iOS)</li>
             <li><a href="https://reederapp.com/">Reeder</a> (macOS, iOS)</li>
             <li><a href="https://www.vienna-rss.com/">Vienna</a> (macOS)</li>
             <li><a href="https://www.microsoft.com/en-us/p/newsflow/9nblggh58s5r">Newsflow</a> (Windows)</li>
-            <li><a href="https://feedly.com/">Feedly</a> (Web Service)</li>
-            <li><a href="https://feedbin.com/">Feedbin</a> (Web Service)</li>
+            <li><a href="https://feedly.com/">Feedly</a> (tīmekļa pakalpojums)</li>
+            <li><a href="https://feedbin.com/">Feedbin</a> (tīmekļa pakalpojums)</li>
         </ul>',
 
-    'images_browse_title' => 'Browse images uploaded for this consultation',
-    'images_none'         => 'No image uploaded yet.',
-    'images_delete'       => 'Delete',
-    'images_deleted'      => 'The image was deleted.',
+    'images_browse_title' => 'Pārlūkot šai apspriešanai augšupielādētos attēlus',
+    'images_none'         => 'Vēl nav augšupielādēts neviens attēls.',
+    'images_delete'       => 'Dzēst',
+    'images_deleted'      => 'Attēls ir dzēsts.',
 
-    'settings_allcons' => 'Site-wide',
-    'settings_inmenu'  => 'Show in menu',
+    'settings_allcons' => 'Visā vietnē',
+    'settings_inmenu'  => 'Rādīt izvēlnē',
     'settings_url'     => 'URL',
-    'settings_title'   => 'Title',
-    'settings_delete'  => 'Delete page',
-    'create_btn'       => 'Create',
-    'menu_add_btn'     => 'Create a new content page in the menu',
-    'fullscr_select'   => 'Content pages',
-    'fullscr_split'    => 'Split screen',
+    'settings_title'   => 'Nosaukums',
+    'settings_delete'  => 'Dzēst lapu',
+    'create_btn'       => 'Izveidot',
+    'menu_add_btn'     => 'Izvēlnē izveidot jaunu satura lapu',
+    'fullscr_select'   => 'Satura lapas',
+    'fullscr_split'    => 'Sadalīts ekrāns',
 
-    'documents_title' => 'Documents',
-    'documents_intro_text' => 'On this page, documents for this consultation are published.',
-    'documents_add_group_btn' => 'New file group',
-    'documents_group_name' => 'Group name',
-    'documents_group_add' => 'Create',
-    'documents_group_delete' => 'Delete file group',
-    'documents_group_delete_c' => 'Do you really want to delete the file group and all its contained files?',
-    'documents_file_delete' => 'Delete file',
-    'documents_file_delete_c' => 'Do you really want to delete this?',
-    'documents_group_added' => 'The file group was added',
-    'documents_group_deleted' => 'The file group was deleted',
-    'documents_file_deleted' => 'The file was deleted',
-    'documents_add_file_btn' => 'Upload new file',
-    'documents_file_add' => 'Upload file',
-    'documents_uploaded_file' => 'The file was uploaded.',
-    'documents_download_all' => 'Download all',
-    'documents_none_yet' => 'No documents were published yet.',
-    'documents_sort' => 'Sort file groups',
+    'documents_title' => 'Dokumenti',
+    'documents_intro_text' => 'Šajā lapā tiek publicēti šīs apspriešanas dokumenti.',
+    'documents_add_group_btn' => 'Jauna failu grupa',
+    'documents_group_name' => 'Grupas nosaukums',
+    'documents_group_add' => 'Izveidot',
+    'documents_group_delete' => 'Dzēst failu grupu',
+    'documents_group_delete_c' => 'Vai tiešām vēlaties dzēst failu grupu un visus tajā esošos failus?',
+    'documents_file_delete' => 'Dzēst failu',
+    'documents_file_delete_c' => 'Vai tiešām vēlaties to dzēst?',
+    'documents_group_added' => 'Failu grupa pievienota',
+    'documents_group_deleted' => 'Failu grupa dzēsta',
+    'documents_file_deleted' => 'Fails dzēsts',
+    'documents_add_file_btn' => 'Augšupielādēt jaunu failu',
+    'documents_file_add' => 'Augšupielādēt failu',
+    'documents_uploaded_file' => 'Fails augšupielādēts.',
+    'documents_download_all' => 'Lejupielādēt visu',
+    'documents_none_yet' => 'Vēl nav publicēts neviens dokuments.',
+    'documents_sort' => 'Kārtot failu grupas',
 
-    'err_permission' => 'No permissions to edit this page',
-    'err_exists_site' => 'There already is a site-wide content page with this URL.',
-    'err_exists_con' => 'There already is a consultation content page with this URL.',
-    'err_invalid_caracter' => 'Invalid character in the URL',
+    'err_permission' => 'Nav atļauju rediģēt šo lapu',
+    'err_exists_site' => 'Ar šo URL jau eksistē vietnes mēroga satura lapa.',
+    'err_exists_con' => 'Ar šo URL jau eksistē apspriešanas satura lapa.',
+    'err_invalid_caracter' => 'Nederīga rakstzīme URL',
 ];
